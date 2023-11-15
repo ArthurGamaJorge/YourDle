2595bcf8e3121d4e344a777abc5d962f80df678c
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-11-15 02:13:28 -0300

    Adicionar página de conexo

---
+++ scripts/conexo.js
@@ -0,0 +1,7 @@
+let mostrarInfo = () => {
+    document.getElementById('divInfo').classList.toggle('aberto')
+}
+
+let fecharInfo = () => {
+    document.getElementById('divInfo').classList.remove('aberto')
+}
