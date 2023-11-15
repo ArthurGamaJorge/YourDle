72c4e6a97db017e2f59758deaf87bbbc387bf1fa
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-11-15 13:17:38 -0300

    Adicionar node e implementar sistema de wordle

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+
+const route = require('./routes/route')
+
+// Decode e uncode de json para objeto e objeto para json
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/', route)
+
+
+// Node escuta requisições da porta 3000
+app.listen(3030, () =>{
+    console.log("Servidor Projeto Node com SQLServer")
+})
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, '../public')));
+
+// COLOCAR ARQUIVOS HTML
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
+app.get("/conexo.html", function(req, res){
+    res.sendFile(path.join(__dirname, '../conexo.html'));
+})
+app.get("/wordle.html", function(req, res){
+    res.sendFile(path.join(__dirname, '../wordle.html'));
+})
+
+const fs = require('fs');
+const readline = require('readline');
+
+app.post("/palavraValida", async(req, res) =>{
+    const readLine = readline.createInterface({
+        input: fs.createReadStream('./words.txt'), // Ajuste o caminho do arquivo conforme necessário
+        output: process.stdout,
+        terminal: false,
+    });
+
+    let found = false;
+
+    for await (const line of readLine) {
+        if (line.trim() === req.body.palavra) {
+            found = true;
+            break;
+        }
+    }
+
+    res.json({resposta: found});
+  })
+
+    
---
+++ src/routes/route.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+
+// publica as rotas
+module.exports = router
