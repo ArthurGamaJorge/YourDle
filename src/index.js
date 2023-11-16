aafd04ff160ae582d072bacb080e8be6dfe37d30
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-11-16 14:05:37 -0300

    Corrigir descrição wordle

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
+
 app.get("/conexo", function(req, res){
     res.sendFile(path.join(__dirname, '../public/conexo.html'));
 })
